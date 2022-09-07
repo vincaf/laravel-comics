4a99778e3c8e9b19cbccb69c2f38ad5bd2800fb5
vincaf <vincenzocafassog@gmail.com>
2022-09-08 00:24:24 +0200

    feat: add variable for comics config

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = config('comics');
+    return view('home', ['comics' => $data]);
 });
