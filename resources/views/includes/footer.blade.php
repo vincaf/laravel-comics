e6b073c21a1539b34db4c220da018725c77d862c
vincaf <vincenzocafassog@gmail.com>
2022-09-07 18:42:47 +0200

    feat: add footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,77 @@
+<footer>
+    <div id="navbar_footer">
+      <div class="container-content d-flex justify-content-between">
+        <div class="list">
+          <div>
+            <h4>DC COMICS</h4>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>SHOP</h4>
+            <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>DC</h4>
+            <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy policy</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>SITES</h4>
+            <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <img src="images/dc-logo-bg.png" alt="">
+        </div>
+
+      </div>
+    </div>
+    <div id="low_footer">
+      <div class="container-content d-flex justify-content-between">
+        <div class="py-4">
+          <button>SIGN-UP NOW!</button>
+        </div>
+
+        <div class="d-flex py-4 align-items-center">
+          <div class="follow-us">FOLLOW US</div>
+          <div class="icon"><img src="images/footer-facebook.png" alt="facebook"></div>
+          <div class="icon"><img src="images/footer-twitter.png" alt="facebook"></div>
+          <div class="icon"><img src="images/footer-youtube.png" alt="facebook"></div>
+          <div class="icon"><img src="images/footer-pinterest.png" alt="facebook"></div>
+          <div class="icon"><img src="images/footer-periscope.png" alt="facebook"></div>
+        </div>
+      </div>
+    </div>
+</footer>
