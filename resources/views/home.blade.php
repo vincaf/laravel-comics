5fe0a4422f004ea087bc52b18879b8b732a88f40
vincaf <vincenzocafassog@gmail.com>
2022-09-07 18:20:56 +0200

    feat: add header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('main-content')
-    
+    <h1>Contenuto</h1>
 @endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<header class="container-content">
+    <div class="d-flex justify-content-between align-items-center">
+        <img src="images/dc-logo.png" alt="dc logo">
+        <ul class="d-flex">
+            <li>CHARACTERS</li>
+            <li class="active">COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEO</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
+</header>
